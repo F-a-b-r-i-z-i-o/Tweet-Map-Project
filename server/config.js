e732ea80759554edd00d9b6f025623b206479776
F-a-b-r-i-z-i-o <44865237+F-a-b-r-i-z-i-o@users.noreply.github.com>
2021-12-03 19:06:45 +0100

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         '98CGciC3IarkRqjNfUokprQil', 
+  consumer_secret:      'I6ibPMNkkgwzYXG5JzxlddlpHpz5Vztio1hM3Hv054gmzvGkwS',
+  access_token:         '1370159895916470272-SBqcWPNauq9vPNGXlbz6GyAVI2dB5f',
+  access_token_secret:  'HxBQIfZqmUQinNkeGUu5M2oscI79ofIyu3xV0kS5PCTRd'
+}
