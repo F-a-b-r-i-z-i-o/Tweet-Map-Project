1f9a760e78d600e15103dbe73f6be0002b65dacf
F-a-b-r-i-z-i-o <44865237+F-a-b-r-i-z-i-o@users.noreply.github.com>
2021-12-03 19:08:52 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         '98CGciC3IarkRqjNfUokprQil', 
-  consumer_secret:      'I6ibPMNkkgwzYXG5JzxlddlpHpz5Vztio1hM3Hv054gmzvGkwS',
-  access_token:         '1370159895916470272-SBqcWPNauq9vPNGXlbz6GyAVI2dB5f',
-  access_token_secret:  'HxBQIfZqmUQinNkeGUu5M2oscI79ofIyu3xV0kS5PCTRd'
+  consumer_key:         'KEY', 
+  consumer_secret:      'KEY',
+  access_token:         'KEY',
+  access_token_secret:  'KEY'
 }
